1de1c32a8539fb949d0434a9486ca1408fc0e1ec
Briltz <jake.briltz12@gmail.com>
2018-11-15 12:17:38 -0500

    Rewroked home page and added functionality to tab buttons

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+
+    constructor(props) {
+	    super(props)
+	    this.slide1 = {
+	      backgroundImage: 'url(/assets/Slide1.png)'
+	    }
+    }
+
     render() {
 	return (
 	    <div>
-		Hello World
+		<header>
+		      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		        <ol class="carousel-indicators">
+		          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		        </ol>
+		        <div class="carousel-inner" role="listbox">
+		          <div class="carousel-item active" style={this.slide1}>
+		          </div>
+		          <div class="carousel-item slide-2">
+		          </div>
+		          <div class="carousel-item slide-3">
+		          </div>
+		        </div>
+		        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		          <span class="sr-only">Previous</span>
+		        </a>
+		        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		          <span class="sr-only">Next</span>
+		        </a>
+		      </div>
+		    </header>
 	    </div>
 	);
     }
