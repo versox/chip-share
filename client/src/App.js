96a39152ca255164d6c7e6c11c5c7430975cc398
versox <cass.carson@gmail.com>
2018-11-17 11:29:23 -0500

    user menu

--- client/src/App.js
+++ client/src/App.js
@@ -19,58 +19,57 @@ class App extends Component {
 		];
 	}
 
-	setActive(index) {
-		this.setState({
-			activePage: index
-		});
-		Cookies.set('actPage', index, { expires: 1 });
-	}
-
 	render() {
-		var navBarItems = this.itemNames.map((name, index) => {
-			return <li className="nav-item" key={index}>
-				<a href={this.destination[index]} className={"nav-link " + (false ? "active" : "")}>
-					{this.itemNames[index]}
-				</a>
-			</li>;
+		const navBarItems = this.itemNames.map((name, index) => {
+		    return <li className="nav-item" key={index}>
+			    <a href={this.destination[index]} className={"nav-link " + (false ? "active" : "")}>
+				    {this.itemNames[index]}
+			    </a>
+		    </li>;
 		});
+		const UserMenu = () => {
+		    var name = Cookies.get('name');
+		    return <ul className="navbar-nav ml-auto">
+			{(name === undefined) ?
+			(<a class="nav-link" href="/login">Log In / Register</a>)
+			    :
+			(<li class="nav-item dropdown">
+			    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Logged in as: {name}
+			    </a>
+			    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				<a class="dropdown-item" href="#">Profile</a>
+				<a class="dropdown-item" href="#">Options</a>
+				<div class="dropdown-divider"></div>
+				<a class="dropdown-item" href="#">Log Out</a>
+			    </div>
+			</li>)}
+			</ul>;
+		}
 		return (
 			<div className="App">
-				<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-					<div className="container">
-						<a className="navbar-brand" href="/">Chip Share</a>
-						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-							<span className="navbar-toggler-icon"></span>
-						</button>
-						<div className="collapse navbar-collapse" id="navbarResponsive">
-							<ul className="navbar-nav mr-auto">
-								{navBarItems}
-							</ul>
-							<ul className="navbar-nav ml-auto">
-								<a class="nav-link" href="/login">Log In / Register</a>
-								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-										Logged in as: Test User
-									</a>
-									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-										<a class="dropdown-item" href="#">Profile</a>
-										<a class="dropdown-item" href="#">Options</a>
-										<div class="dropdown-divider"></div>
-										<a class="dropdown-item" href="#">Log Out</a>
-									</div>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</nav>
 				<Router>
-					<div>
-						<Route path="/" exact component={Home} />
-						<Route path="/editor" component={Editor} />
-						<Route path="/login" component={Login} />
-						<Route path="/register" component={Register} />
-						<Route path="/profile" component={Profile} />
-					</div>
+				    <div>
+					<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+						<div className="container">
+							<a className="navbar-brand" href="/">Chip Share</a>
+							<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+								<span className="navbar-toggler-icon"></span>
+							</button>
+							<div className="collapse navbar-collapse" id="navbarResponsive">
+								<ul className="navbar-nav mr-auto">
+									{navBarItems}
+								</ul>
+								<Route path="/" component={UserMenu} />
+							</div>
+						</div>
+					</nav>
+					<Route path="/" exact component={Home} />
+					<Route path="/editor" component={Editor} />
+					<Route path="/login" component={Login} />
+					<Route path="/register" component={Register} />
+					<Route path="/profile" component={Profile} />
+				    </div>
 				</Router>
 			</div>
 		);
