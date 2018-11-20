8c3eb88b23be51dd7d7d14fd78390674a8a187cb
versox <cass.carson@gmail.com>
2018-11-20 12:08:46 -0500

    made server serve static builds

---
+++ client/build/precache-manifest.b000fe37d861a65d8464d6e72a2c010f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9660a2a9ed707ac8cc8",
+    "url": "/static/js/main.f9660a2a.chunk.js"
+  },
+  {
+    "revision": "cf119ed79a4085d7b68a",
+    "url": "/static/js/1.cf119ed7.chunk.js"
+  },
+  {
+    "revision": "f9660a2a9ed707ac8cc8",
+    "url": "/static/css/main.72b26275.chunk.css"
+  },
+  {
+    "revision": "f3f2911973b2ec584ffa7b534ad7b035",
+    "url": "/index.html"
+  }
+];
--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 const settings = require('./lib/config/config');
 mongoose.connect(settings.databaseUrl || 'mongodb://localhost/chip-share', { useNewUrlParser: true });
 
+app.use(express.static("../client/build"));
+
 app.use(express.json()); // only parse json input
 
 // routing
 app.use("/api", require("./lib/routes/api_router"));
+
+// react routing
+app.get('*', (req,res) =>{
+    res.sendFile('client/build/index.html', { root: path.join('__dirname', '/../../')});
+});
 
 // handle errors
 require('./bin/error_handler')(app);
