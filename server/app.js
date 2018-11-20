41d952d30c02c9dbabafb830cb1b3b65d72f913e
Alex Yaro <alxyaro@gmail.com>
2018-11-20 13:19:06 -0500

    Use absolute paths for serving client

--- server/app.js
+++ server/app.js
@@ -1,20 +1,19 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 const settings = require('./lib/config/config');
 mongoose.connect(settings.databaseUrl || 'mongodb://localhost/chip-share', { useNewUrlParser: true });
 
-app.use(express.static("../client/build"));
-
 app.use(express.json()); // only parse json input
 
 // routing
-app.use("/api", require("./lib/routes/api_router"));
+app.use('/api', require('./lib/routes/api_router'));
 
-// react routing
-app.get('*', (req,res) =>{
-    res.sendFile('client/build/index.html', { root: path.join('__dirname', '/../../')});
+const clientBuildDir = path.resolve(__dirname, '../client/build');
+app.use(express.static(clientBuildDir));
+app.get('*', (req, res) => {
+	res.sendFile('index.html', { root: clientBuildDir});
 });
 
 // handle errors
