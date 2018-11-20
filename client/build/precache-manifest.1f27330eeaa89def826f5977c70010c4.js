7f8292c7b02d7feb80b077e1b3f35d3c8b106185
versox <cass.carson@gmail.com>
2018-11-20 14:26:09 -0500

    forgot to rebuild

--- client/build/precache-manifest.1f27330eeaa89def826f5977c70010c4.js
+++ client/build/precache-manifest.1f27330eeaa89def826f5977c70010c4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ryerson/chip-share/static/js/runtime~main.cb495f82.js"
   },
   {
-    "revision": "c41883d7963cc85b0ef4",
-    "url": "/ryerson/chip-share/static/js/main.c41883d7.chunk.js"
+    "revision": "13a9cb72bbe5e9e388a3",
+    "url": "/ryerson/chip-share/static/js/main.13a9cb72.chunk.js"
   },
   {
     "revision": "9c7e17277cb01905997d",
     "url": "/ryerson/chip-share/static/js/1.9c7e1727.chunk.js"
   },
   {
-    "revision": "c41883d7963cc85b0ef4",
+    "revision": "13a9cb72bbe5e9e388a3",
     "url": "/ryerson/chip-share/static/css/main.72b26275.chunk.css"
   },
   {
-    "revision": "c518305f5105dd23410e4d38c1274845",
+    "revision": "2505441af268aea9e080e376ea945617",
     "url": "/ryerson/chip-share/index.html"
   }
 ];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ryerson/chip-share/precache-manifest.9cfd8b34bd79fdfbc10e2d5cd739ce39.js"
+  "/ryerson/chip-share/precache-manifest.1f27330eeaa89def826f5977c70010c4.js"
 );
 
 workbox.clientsClaim();
