006b86f46b9f6fb7446932b4c0266180acba6955
Alex Yaro <alxyaro@gmail.com>
2018-11-22 01:45:47 -0500

    Un-gitignore /build dir (thanks carson, i could have been in bed 30 minutes ago)

---
+++ client/build/precache-manifest.caa9b5f8b64564b5dd664a09bae24552.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90c61e648e0e50a34af6",
+    "url": "/static/js/main.90c61e64.chunk.js"
+  },
+  {
+    "revision": "282dc8293e2ec826b121",
+    "url": "/static/js/1.282dc829.chunk.js"
+  },
+  {
+    "revision": "90c61e648e0e50a34af6",
+    "url": "/static/css/main.63e028f7.chunk.css"
+  },
+  {
+    "revision": "273433caaa21670ae6b02dd63ca9f1c6",
+    "url": "/index.html"
+  }
+];
