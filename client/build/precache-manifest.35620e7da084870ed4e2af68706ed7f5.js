c36e11651ff79ceb0c8c29d39ed6d39f3220cf91
Alex Yaro <alxyaro@gmail.com>
2018-12-02 01:22:19 -0500

    Build

---
+++ client/build/precache-manifest.35620e7da084870ed4e2af68706ed7f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2715a497fb975a105d0e",
+    "url": "/static/js/main.2715a497.chunk.js"
+  },
+  {
+    "revision": "1ba3e7e6910bd77a3539",
+    "url": "/static/js/1.1ba3e7e6.chunk.js"
+  },
+  {
+    "revision": "2715a497fb975a105d0e",
+    "url": "/static/css/main.5d83812b.chunk.css"
+  },
+  {
+    "revision": "1ba3e7e6910bd77a3539",
+    "url": "/static/css/1.558dcac3.chunk.css"
+  },
+  {
+    "revision": "41b781ec0dc9f659a652bf21145bf28c",
+    "url": "/index.html"
+  }
+];
