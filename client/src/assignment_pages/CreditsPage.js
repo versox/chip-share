02e8779ae7fd12a5dab531030ccd3aae4c951c3d
Alex Yaro <alxyaro@gmail.com>
2018-12-03 16:10:35 -0500

    Added frameworks page content, various minor tweaks.

--- client/src/assignment_pages/CreditsPage.js
+++ client/src/assignment_pages/CreditsPage.js
@@ -15,7 +15,6 @@ class CreditsPage extends Component {
 					<div className="col-lg-3 col-sm-6 col-8 credits-block">
 						<img src="https://avatars2.githubusercontent.com/u/44684541?s=400" />
 						<h3>Alexander</h3>
-						<a href="https://github.com/alxyaro" title="Github Account">github.com/alxyaro</a>
 						<ScrollArea horizontal={false} speed={0.8}>
 							<ul>
 								<li>Created the entirety of the backend REST API for Chip Share.</li>
@@ -34,7 +33,6 @@ class CreditsPage extends Component {
 					<div className="col-lg-3 col-sm-6 col-8 credits-block">
 						<img src="https://avatars3.githubusercontent.com/u/6993228?s=400" />
 						<h3>Carson</h3>
-						<a href="https://github.com/versox" title="Github Account">github.com/versox</a>
 						<ScrollArea horizontal={false} speed={0.8}>
 							<ul>
 								<li>Created the <Link to="/editor/new">editor</Link>.</li>
@@ -49,28 +47,25 @@ class CreditsPage extends Component {
 					<div className="col-lg-3 col-sm-6 col-8 credits-block">
 						<img src="https://avatars2.githubusercontent.com/u/44612512?s=400" />
 						<h3>Jake</h3>
-						<a href="https://github.com/briltz" title="Github Account">github.com/briltz</a>
 						<ScrollArea horizontal={false} speed={0.8}>
 							<ul>
-								<li>Created installation page, conclusion page, and helped with frameworks page</li>
-								<li>Prototyped navbar, home page, and profile page</li>
-								<li>Styled song editor, aided with main style and page layout</li>
-								<li>Created carousel on home page</li>
+								<li>Created installation page, conclusion page, and helped with frameworks page.</li>
+								<li>Prototyped navbar, home page, and profile page.</li>
+								<li>Styled song editor, aided with main style and page layout.</li>
+								<li>Created carousel on home page.</li>
 							</ul>
 						</ScrollArea>
 					</div>
 					<div className="col-lg-3 col-sm-6 col-8 credits-block">
 						<img src="https://avatars0.githubusercontent.com/u/45542192?s=400" />
 						<h3>Mishel</h3>
-						<a href="https://github.com/galperinmishel" title="Github Account">github.com/galperinmishel</a>
 						<ScrollArea horizontal={false} speed={0.8}>
 							<ul>
-								<li>Lorem ipsum dolor sit amet, ex movet putent has</li>
-								<li>Lorem ipsum dolor sit amet, ex movet putent has</li>
-								<li>Lorem ipsum dolor sit amet, ex movet putent has</li>
-								<li>Lorem ipsum dolor sit amet, ex movet putent hash</li>
-								<li>Lorem ipsum dolor sit amet, ex movet putent has</li>
-								<li>Lorem ipsum dolor sit amet, ex movet putent has</li>
+								<li>Conducted research on the frameworks and created the frameworks page.</li>
+								<li>Worked on a tutorial for the song editor.</li>
+								<li>Helped with quality assurance of site usability.</li>
+								<li>Assisted in adding demo content for Chip Share.</li>
+								<li>Learned various new foreign concepts used throughout the project.</li>
 							</ul>
 						</ScrollArea>
 					</div>
--- client/src/assignment_pages/FrameworksPage.js
+++ client/src/assignment_pages/FrameworksPage.js
@@ -7,35 +7,25 @@ class FrameworksPage extends Component {
 				<div className="row">
 					<div className="col-12">
 						<h1>Frameworks</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa tincidunt dui ut ornare. Viverra aliquet eget sit amet tellus. Duis convallis convallis tellus id interdum velit laoreet.</p>
-						<h2>Express</h2>
-						<p>Risus feugiat in ante metus dictum. Semper feugiat nibh sed pulvinar. Cras semper auctor neque vitae tempus quam. Cras semper auctor neque vitae tempus quam pellentesque. Viverra justo nec ultrices dui sapien.</p>
-						<ul>
-							<li>Express is one of the most supported Node.js frameworks. It has an open-source community.</li>
-							<li>Express is well-documented.</li>
-							<li>Express id easy to learn.</li>
-                                <li>The code organization in Express is represented by patterns that make your code easier to maintain.</li>
-						</ul>
-						<p>Tristique magna sit amet purus gravida quis blandit. Odio tempor orci dapibus ultrices in iaculis nunc. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio.</p>
-						<ul>
-							<li>If all the code is written in callbacks, it will entangle the code, and it will be hard to maintain.</li>
-							<li>Drawback 2</li>
-							<li>Drawback 3</li>
-						</ul>
+						<p>For our project, two major frameworks were used, namely React and Express, both stemming from the Node.js environment. The following is a brief explanation of both frameworks.</p>
 						<h2>React</h2>
-						<p>Risus feugiat in ante metus dictum. Semper feugiat nibh sed pulvinar. Cras semper auctor neque vitae tempus quam. Cras semper auctor neque vitae tempus quam pellentesque. Viverra justo nec ultrices dui sapien.</p>
-						<ul>
-							<li>Virtual Document Object Model, that allows arranging documents in HTML format into a tree from which is better acceptable by web browsers while parsing different elements of the web app.</li>
-							<li>100% open source JavaScript library which get everyday updates and improvements according to the contributions of developers all over the world.</li>
-                                <li>Light-weighted because the data performing on the user side can be easily represented on the server side simultaneously.</li>
-                                <li>Benefit 3</li>
-                                <li>Benefit 3</li>
-						</ul>
-						<p>Tristique magna sit amet purus gravida quis blandit. Odio tempor orci dapibus ultrices in iaculis nunc. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio.</p>
-						<ul>
-							<li>React JS requires deep knowledge</li>
-							<li>Takes time to learn</li>
-							<li>Drawback 3</li>
+						<p>
+							React is a flexible and efficient front end javascript library for building UI, and is considered the most in demand in the job markets of 2018.<br />
+							It's also considered as one of the most popular javascript frameworks these days.<br />
+							Big companies like Twitter, Netflix, Instagram and Airbnb are using React framework.
+						</p>
+						<h2>Express</h2>
+						<p>
+							Express.js is among the top Node.js frameworks.<br />
+							Express framework has a minimalist outline that allows the developer to take control of each project and how it will be organized. This provides great flexibility but can be a bit overwhelming to those who are new to Node.js.<br />
+							Express is utilized in many major corporations, including but not limited to IBM, Uber, Fox Sports, Mulesoft, etc.
+						</p>
+						<h4>References</h4>
+						<ul style={{fontSize: '12px', marginBottom: '10px'}}>
+							<li>Kostrzewa, Denis. "Is React.js the Best Javascript Framework in 2018? – Hacker Noon." Hacker Noon, Hacker Noon, 19 July 2018, hackernoon.com/is-react-js-the-best-javascript-framework-in-2018-264a0eb373c8.</li>
+							<li>Park, Jae Sung. "The Status of JavaScript Libraries & Frameworks: 2018 & beyond." Hacker Noon, Hacker Noon, 30 Mar. 2018, hackernoon.com/the-status-of-javascript-libraries-frameworks-2018-beyond-3a5a7cae7513.</li>
+							<li>Volodymyr, Tymets. "Express.js Mobile App Development: Pros and Cons of Node.js Framework." Apiko Blog, Apiko Blog, 22 Mar. 2018, apiko.com/blog/express-mobile-app-development/.</li>
+							<li>"Why Developers like React." StackShare, stackshare.io/react/in-stacks.</li>
 						</ul>
 						<hr />
 						<a href="/assignment/installation" className="btn btn-lg btn-primary">Continue to Installation</a>
